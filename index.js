bb13fbaaf8bb714945467f508e3965310fd3ca03
einargs <einargs@gmail.com>
2018-04-01 21:14:13 -0500

    Cleaned up unusued npm packages and added motor commands.

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const SerialPort = require("serialport");
-const socketIO = require("socket.io");
-const express = require("express");
 
 const { connectToArduino } = require("./arduino");
 const { buildIotClient } = require("./iot-cloud");
@@ -17,10 +15,27 @@ const iotClientConfig = {
   deviceId: "arm-1",
   privateKeyFile: "/home/einargs/Auth/armageddon-pi/rsa_private.pem", //TEMP
   algorithm: "RS256",
-  expireSeconds: 2 * 60, //TEMP
+  expireSeconds: 0.5 * 60, //TEMP: currently *ridiculously* short
   mqttBridgeHostname: "mqtt.googleapis.com",
   mqttBridgePort: 8883,
 };
+
+// Format a LED command for the arduino.
+function formatLedCmd(ledId, state) {
+  return `LT:${ledId}:${state?"on":"off"};`;
+}
+
+// Format a motor command for the arduino.
+function formatMotorCmd({
+    base, shoulder, elbow, horizontal, vertical, rotation}) {
+  return `MC:${
+      base}:${
+      shoulder}:${
+      elbow}:${
+      horizontal}:${
+      vertical}:${
+      rotation};`;
+}
 
 async function run() {
   // Setup arduino and iotClient
@@ -29,15 +44,26 @@ async function run() {
 
   // Handle config updates
   iotClient.on("config", (config) => {
+    // Log the config object (and type to catch JSON mixups)
+    console.log("config", typeof config, config);
+
+    // Set the LED states
+    //TODO: see how multiple messages being sent in parallel plays out.
+    // If this is triggered while a motor command is being sent, what happens?
     for (const [ledId, state] of Object.entries(config.leds)) {
-      arduino.send(`LT:${ledId}:${state?"on":"off"};`);
+      arduino.send(formatLedCmd(ledId, state));
     }
+
+    // Set the motor states
+    arduino.send(formatMotorCmd(config.motors));
   });
 }
 
 async function testIotClient() {
   const iotClient = await buildIotClient(iotClientConfig);
-  iotClient.on("config", console.log);
+  iotClient.on("config", (config) => {
+    console.log("Config", typeof config, config);
+  });
 }
-testIotClient();
-//run();
+//testIotClient();
+run();
